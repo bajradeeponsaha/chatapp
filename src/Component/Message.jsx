ad3dda7db11678721241f7b419369283588e2563
bajradeeponsaha <bajradeeponsaha5@gmail.com>
2023-06-29 22:54:49 +0530

    Second Commit

---
+++ src/Component/Message.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { HStack, Avatar, Text } from '@chakra-ui/react'
+
+const Message = ({text, uri, user="other"}) => {
+  return (
+    <HStack alignSelf={user==="me"?"flex-end":"flex-start"} bg={"gray.100"} borderRadius={"base"} paddingX={user==="me"?"4":"2"} paddingY={"2"}>
+        {
+            user==="other" && <Avatar src={uri}></Avatar>
+        }
+        <Text>{text}</Text>
+        {
+            user==="me" && <Avatar src={uri}></Avatar>
+        }
+    </HStack>
+  )
+}
+
+export default Message
